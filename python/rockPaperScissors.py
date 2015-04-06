da9bf00724f201b2d1a8e713e4e046a94fe77136
doni brysoni mbaga <donibrysonimbaga@gmail.com>
2015-04-06 07:11:44 +0300

    Update rockPaperScissors.py

--- python/rockPaperScissors.py
+++ python/rockPaperScissors.py
@@ -3,33 +3,26 @@ userChoice = raw_input("is rock, paper or a scissors? ")
 choices = ["rock", "paper", "scissors"]
 computerChoice = random.choice(choices)
 if computerChoice == userChoice:
-     print "Computer choice was " + computerChoice
-     print "Your choice was " + userChoice
-     print "The result is a tie"
+     print "Computer choice was " + computerChoice +"\n"\
+     + "Your choice was " + userChoice + "\n" + "The result is a tie"
 elif computerChoice == "rock":
      if userChoice == "paper":
-         print "Computer choice was " + computerChoice
-         print "Your choice was " + userChoice
-         print "You win, computer lost"
+         print "Computer choice was " + computerChoice + "\n"\
+          + "Your choice was " + userChoice + "\n" + "You win, computer lost"
      else:
-         print "Computer choice was " + computerChoice
-         print "Your choice was " + userChoice
-         print "Computer wins, you lost"
+         print "Computer choice was " + computerChoice + "\n"\
+          + "Your choice was " + userChoice + "\n" + "Computer wins, you lost"
 elif computerChoice == "scissors":
      if userChoice == "paper":
-         print "Computer choice was " + computerChoice
-         print "Your choice was " + userChoice
-         print "Computer wins, you lost"
+        print "Computer choice was " + computerChoice + "\n"\
+          + "Your choice was " + userChoice + "\n" + "Computer wins, you lost"
      else:
-         print "Computer choice was " + computerChoice
-         print "Your choice was " + userChoice
-         print "You win, computer lost"
+          print "Computer choice was " + computerChoice + "\n"\
+          + "Your choice was " + userChoice + "\n" + "You win, computer lost"
 elif computerChoice == "paper":
     if userChoice == "rock":
-         print "Computer choice was " + computerChoice
-         print "Your choice was " + userChoice
-         print "Computer wins, you lost"
+         print "Computer choice was " + computerChoice + "\n"\
+         + "Your choice was " + userChoice + "\n" + "Computer wins, you lost"
     else:
-         print "The computer choice was " + computerChoice
-         print "Your choice was " + userChoice
-         print "You wins, Computer lost"
+          print "The computer choice was " + computerChoice + "\n"\
+         + "Your choice was " + userChoice + "\n" + "You wins, Computer lost"
